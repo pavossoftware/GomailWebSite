d59a5b64114e29a405f42e1a29cd00244b23654c
developer <github@pavos.com.tr>
2019-06-19 21:39:14 +0300

    linkler https yapıldı
    linkler https yapıldı

--- assets/js/services/validateService.js
+++ assets/js/services/validateService.js
@@ -3,22 +3,22 @@ angular.module('validateService', [])
 	.factory('validateSrv', ['$http',function($http) {
 		return {
 			isvalit : function(email) {
-				return $http.get('http://maildogrulama.com/api/stringvalidate/' + email);
+				return $http.get('https://maildogrulama.com/api/stringvalidate/' + email);
 			},
 			ckdomain : function(domain) {
-				return $http.get('http://maildogrulama.com/api/checkdisposableDomains/' + domain);
+				return $http.get('https://maildogrulama.com/api/checkdisposableDomains/' + domain);
 			},
 			mxvalit : function(domain) {
-				return $http.get('http://maildogrulama.com/api/mxvalidate/' + domain);
+				return $http.get('https://maildogrulama.com/api/mxvalidate/' + domain);
 			},
 			smtpvalit : function(domain) {
-				return $http.get('http://maildogrulama.com/api/smtpvalidate/' + domain);
+				return $http.get('https://maildogrulama.com/api/smtpvalidate/' + domain);
 			},
 			timerCount : function() {
-				return $http.get('http://maildogrulama.com/api/timerCount/',{headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' } });
+				return $http.get('https://maildogrulama.com/api/timerCount/',{headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' } });
 			},
 			IPtimercount : function() {
-				return $http.get('http://maildogrulama.com/api/IPtimercount/');
+				return $http.get('https://maildogrulama.com/api/IPtimercount/');
 			}
 		}
 	}]);
